97a71528c15f956968894c01996583e791a0ff1a
Dawid Paliczuk <d.paliczuk@live.de>
2018-06-04 12:29:33 +0200

    form definition

---
+++ fes-gui-wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fes_gui_wpf.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
